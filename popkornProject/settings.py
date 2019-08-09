2106a40e5094c7d963870a9deb9ef043eaf59150
ji <alslahdk@naver.com>
2019-08-09 16:39:49 +0900

    prepare for heroku

--- popkornProject/settings.py
+++ popkornProject/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +135,8 @@ STATIC_DIR = [os.path.join(BASE_DIR, 'popkornApp', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
